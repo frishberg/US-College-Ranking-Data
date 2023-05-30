3f2307eb07054001aca27bec2e53a7c15a4eca74
A-ron <aronf32@gmail.com>
2023-05-30 01:33:04 -0400

    adding 2023 US News rankings to the spreadsheets and data

--- playground.py
+++ playground.py
@@ -2,27 +2,15 @@ import csv
 import os
 import json
 
-csv_writer = csv.writer(open('data/all_data.csv', 'w'), delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-headers = ["University Name", "Acceptance Rate", "Average SAT", "Niche Academic Grade", "Niche Athletic Grade", "Niche Campus Grade", "Niche Campus Food Grade", "Niche Diversity Grade", "Niche Dorms Grade", "Niche Location Grade", "Niche Overall Grade", "Niche Party Scene Grade", "Niche Professors Grade", "Niche Safety Grade", "Niche Student Life Grade", "Niche Value Grade", "Nobel Prizes", "QS World Rankings", "Undergraduate Population"]
-csv_writer.writerow(headers)
-        
-
+f = open("external-data/historical-usnews.json", "r")
+data = json.load(f)
+f.close()
 
 f = open("Included Schools.txt", "r")
 schools = f.readlines()
 f.close()
 
 for school in schools :
-    school_data = []
-    school_data.append(school.strip())
-    for filename in os.listdir("data") :
-        if (filename == "all_data.csv") :
-            continue
-        f = open("data/" + filename, "r")
-        data = json.load(f)
-        f.close()
-        if (school.strip() not in data) :
-            school_data.append("")
-        else :
-            school_data.append(data[school.strip()])
-    csv_writer.writerow(school_data)
+    school = school.strip()
+    rank = str(data[school]['2023'])
+    print(rank)
