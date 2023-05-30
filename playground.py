4c70f6116cf2459487312c18cb6f8f6ce300960b
A-ron <aronf32@gmail.com>
2023-05-30 02:03:35 -0400

    adding json file with all of the data for each school

--- playground.py
+++ playground.py
@@ -1,15 +1,28 @@
 import csv
-import os
 import json
 
-f = open("json-data/2022_qs_world_ranking.json", "r")
-data = json.load(f)
+f = open('data.csv', 'r')
+reader = csv.reader(f)
+headers = ['University Name', 'Acceptance Rate', 'Average SAT', 'Niche Academic Grade', 'Niche Athletic Grade', 'Niche Campus Grade', 'Niche Campus Food Grade', 'Niche Diversity Grade', 'Niche Dorms Grade', 'Niche Location Grade', 'Niche Overall Grade', 'Niche Party Scene Grade', 'Niche Professors Grade', 'Niche Safety Grade', 'Niche Student Life Grade', 'Niche Value Grade', 'Nobel Prizes', 'QS World Rankings', 'Undergraduate Population', '2023 US News Ranking', '2022 QS World Ranking']
+
+json_data = {}
+
+def toInt(s) :
+    #if the string can be a number
+    if (s.isdigit()) :
+        return int(s)
+    else :
+        return s
+
+for row in reader:
+    if (row[0] == 'University Name'):
+        continue
+    name = row[0]
+    json_data[name] = {}
+    for i in range(1, len(headers)):
+        json_data[name][headers[i]] = toInt(row[i])
 f.close()
 
-f = open("Included Schools.txt", "r")
-schools = f.readlines()
+f = open("data.json", "w")
+f.write(json.dumps(json_data))
 f.close()
-
-for school in schools :
-    school = school.strip()
-    print(data[school])
