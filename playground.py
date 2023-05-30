da8e0247a4f113519c70fd6f00c3a19688efe2d6
A-ron <aronf32@gmail.com>
2023-05-30 02:08:20 -0400

    correcting the nobel prizes json so that the values are integers instead of strings

--- playground.py
+++ playground.py
@@ -1,28 +1,16 @@
-import csv
 import json
 
-f = open('data.csv', 'r')
-reader = csv.reader(f)
-headers = ['University Name', 'Acceptance Rate', 'Average SAT', 'Niche Academic Grade', 'Niche Athletic Grade', 'Niche Campus Grade', 'Niche Campus Food Grade', 'Niche Diversity Grade', 'Niche Dorms Grade', 'Niche Location Grade', 'Niche Overall Grade', 'Niche Party Scene Grade', 'Niche Professors Grade', 'Niche Safety Grade', 'Niche Student Life Grade', 'Niche Value Grade', 'Nobel Prizes', 'QS World Rankings', 'Undergraduate Population', '2023 US News Ranking', '2022 QS World Ranking']
-
-json_data = {}
-
-def toInt(s) :
-    #if the string can be a number
-    if (s.isdigit()) :
-        return int(s)
-    else :
-        return s
-
-for row in reader:
-    if (row[0] == 'University Name'):
-        continue
-    name = row[0]
-    json_data[name] = {}
-    for i in range(1, len(headers)):
-        json_data[name][headers[i]] = toInt(row[i])
+f = open("json-data/nobel_prizes.json", "r")
+data = json.load(f)
 f.close()
 
-f = open("data.json", "w")
-f.write(json.dumps(json_data))
+new_data = {}
+
+for row in data :
+    name = row
+    val = int(data[row])
+    new_data[name] = val
+
+f = open("json-data/nobel_prizes.json", "w")
+json.dump(new_data, f)
 f.close()
